6ce4b07a88472cee501ce66cd89f0d54b2a5effa
marcin <mbbigblue@gmail.com>
2022-03-22 11:43:18 +0100

    adding measuring serializer

---
+++ smart_home_cockpit/measurement.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+import json
+
+
+@dataclass
+class MeasurementRecord:
+    id: str
+    name: str
+    location: str
+    timestamp: str
+    user: str
+    active: bool
+    value: float
+    scale: str
+    comment: str
+
+
+record = MeasurementRecord(id="123f",
+                           location="New York City",
+                           timestamp="10-04-19 12:00:17",
+                           user="Jack Strong",
+                           active=True,
+                           name="Temperature",
+                           value=17.3,
+                           scale="C",
+                           comment="Basement"
+                           )
+
+
+jsonStr = json.dumps(record.__dict__)
+
+#print json string
+print(jsonStr)
